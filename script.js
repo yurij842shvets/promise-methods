6ac28e85e8024206e3d9c1ff427bfe7196ae2594
yurij842shvets <yurij2011shvets@gmail.com>
2025-02-15 20:41:02 +0200

    add async functions allPromises and racePromises

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
-// function delayedPromise(value, delay) {
-//     return new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(value)
-//         }, delay)
-//     })
-// }
-// const promises = [
-//     delayedPromise('first promise', 3000),
-//     delayedPromise('second promise', 1500),
-//     delayedPromise('third promise', 2000),
-//     delayedPromise('fourth promise', 500),
-//     delayedPromise('fifth promise', 4500)
-// ]
-
-// Promise.all(promises)
-//     .then((result) => console.log('promises worked', result))
-//     .catch((error) => console.log('error', error))
-
-
-
-
-
-
-function randomDelay(value, delay) {
+function delayedPromise(value, delay) {
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(value)
         }, delay)
     })
 }
-
 const promises = [
-    randomDelay('first promise', 3000),
-    randomDelay('second promise', 1500),
-    randomDelay('third promise', 2000),
-    randomDelay('fourth promise', 500),
-    randomDelay('fifth promise', 4500)
+    delayedPromise('first promise', 3000),
+    delayedPromise('second promise', 1500),
+    delayedPromise('third promise', 2000),
+    delayedPromise('fourth promise', 500),
+    delayedPromise('fifth promise', 4500)
 ]
+async function allPromises() {
+    try {
+        const result = await Promise.all(promises)
+        console.log('promises worked', result)
+    } catch (error) {
+        console.log('error', error)
+    }
+}
+allPromises()
 
-Promise.race(promises)
-    .then((result) => console.log(result, 'won'))
-    .catch((error) => console.log('error', error))
+
+
+
+
+// function randomDelay(value) {
+//     const delay = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(value);
+//         }, delay);
+//     });
+// }
+
+// const promises = [
+//     randomDelay('first promise'),
+//     randomDelay('second promise'),
+//     randomDelay('third promise'),
+//     randomDelay('fourth promise'),
+//     randomDelay('fifth promise')
+// ]
+
+// async function racePromises() {
+//     try {
+//         const result = await Promise.race(promises)
+//         console.log(result, 'won')
+//     } catch (error) {
+//         console.log('error', error)
+//     }
+// }
+// racePromises()
